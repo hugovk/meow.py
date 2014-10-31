62f8c8162071de758ecd7081fe0c1ce4da457d7a
hugovk <hugovk@users.noreply.github.com>
2014-11-01 00:48:46 +0200

    meow.py

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 meow.py
 =======
+
+An entry for [NaNoGenMo 2014](https://github.com/dariusk/NaNoGenMo-2014/):
+
+> Spend the month of November writing code that generates a novel of 50k+ words.
+
+The rules say:
+
+> The "novel" is defined however you want. It could be 50,000 repetitions of the word "meow". It could literally grab a random novel from Project Gutenberg. It doesn't matter, as long as it's 50k+ words.
+
+Well, someone had better create 50,000 meows just to get it out of the way.
+
+But this isn't just "meow" 50,000 times. meow.py replaces all words with a meow of the same length, keeping punctuation.
+
+Usage:
+
+`python meow.py infile.txt > outfile.txt"
+
+Here's the above rule put through meow.py:
+
+> Mew "meeow" me meeooow meoooow mew meow. Me meeow me me,mew meeeeeoooow me mew meow "meow". Me meoow meeeeeoow meow m meooow meoow meow Meeooow Meeeeeoow. Me meoow'm meooow, me meow me me'm mew+ meoow.
+
---
+++ meow.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+"""
+Replace all words with meows, preserving punctuation.
+"""
+from __future__ import print_function, unicode_literals
+
+import re
+import sys
+import random
+import argparse
+
+
+def is_word(thing):
+    found = re.match(r"\w+", thing, re.UNICODE)
+    return found
+
+
+def meow_meow(line):
+    """Meowify a line"""
+    meowed = []
+    # Break line into words and non-words (e.g. punctuation and space)
+    things = re.findall(r"\w+|[^\w]", line, re.UNICODE)
+    for thing in things:
+        if is_word(thing):
+            meowed.append(meow(thing))
+        else:
+            meowed.append(thing)
+    return u"".join(meowed)
+
+
+def meow(word):
+    """Meowify a word"""
+    meowed = ""
+    length = len(word)
+
+    if length == 1:
+        return capify("m", word)
+    elif length == 2:
+        return capify("me", word)
+    elif length == 3:
+        return capify("mew", word)
+    elif length == 4:
+        return capify("meow", word)
+
+    # Words longer than four will have:
+    #  * first letter M
+    #  * last letter W
+    #  * middle with a random number of Es, then some Os
+
+    # Number of EOs:
+    eeohs = length - len("m") - len("w")
+    # Number of Es:
+    ees = random.randrange(1, eeohs)
+    # Number of Os:
+    ohs = eeohs - ees
+
+    meowed = "m" + ("e" * ees) + ("o" * ohs) + "w"
+    return capify(meowed, word)
+
+
+def capify(word, reference):
+    """Make sure word has the same capitalisation as reference"""
+    new_word = ""
+
+    # First check whole word before char-by-char
+    if reference.islower():
+        return word.lower()
+    elif reference.isupper():
+        return word.upper()
+
+    # Char-by-char checks
+    for i, c in enumerate(reference):
+        if c.isupper():
+            new_word += word[i].upper()
+        else:
+            new_word += word[i]
+    return new_word
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Replace all words with meows, preserving punctuation.")
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
+                        default=sys.stdin, help="Input text")
+    parser.add_argument('-t', '--translation', action="store_true",
+                        help="Output a line-by-line translation")
+    args = parser.parse_args()
+
+#     for line in fileinput.input(openhook=fileinput.hook_encoded("utf-8")):
+    for line in args.infile:
+        line = line.decode("utf-8-sig").rstrip()  # No BOM
+        if args.translation:
+            print()
+            print(line.encode("utf-8"))
+        print(meow_meow(line).encode("utf-8"))
+
+# End of file
